85336162d9fad64c6f8f103a87b74d749d378787
marasama <abdullah.durusoy31@gmail.com>
2024-01-29 18:25:02 +0300

    First Changes

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -1,43 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adurusoy <adurusoy@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/29 00:38:57 by adurusoy          #+#    #+#             */
-/*   Updated: 2024/01/29 18:14:54 by adurusoy         ###   ########.fr       */
+/*   Created: 2024/01/29 18:24:14 by adurusoy          #+#    #+#             */
+/*   Updated: 2024/01/29 18:24:44 by adurusoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char **argv)
 {
-	int i = 1;
-	int j;
 
-	if(argc < 2)
-	{
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
-		return(0);
-	}
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j] != '\0')
-		{
-			if (argv[i][j] < 123 && argv[i][j] > 96)
-				cout << (char)(argv[i][j] - 32);
-			else
-				cout << argv[i][j];
-			j++;
-		}
-		cout << ' ';
-		i++;
-	}
-	cout << endl;
-	return (0);
 }

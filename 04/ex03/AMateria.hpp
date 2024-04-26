b3625794228bab22aaccb2339f44bddf39bbbcf0
Abdullah ibrahim Durusoy <adurusoy@k2m18s01.42kocaeli.com.tr>
2024-04-27 00:12:24 +0300

    adsdad

---
+++ 04/ex03/AMateria.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+class AMateria
+{
+	protected:
+	public:
+	AMateria(std::string const & type);
+	std::string const &getType() const;
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+	virtual ~AMateria();
+};

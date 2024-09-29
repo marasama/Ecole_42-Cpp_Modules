73f7d69168501febf1807908cc06962ee6966aea
cIlGiN_KoD_YaZar <abdullah.durusoy31@gmail.com>
2024-09-29 21:05:10 +0300

    asdasd

--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -2,13 +2,43 @@
 
 int main()
 {
-	Span a(50);
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	try
+	{
+		Span sp = Span(4);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-	a.addNumber(414);
-	a.addNumber(313);
-	a.addNumber(23);
-	a.addNumber(2);
-
-	std::cout << a.longestSpan() << std::endl;
-	std::cout << a.shortestSpan() << std::endl;
+	try
+	{
+		Span sp = Span(3);
+		sp.addNumber(6);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return 0;
 }
